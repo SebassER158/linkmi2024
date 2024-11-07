1be4026aeaaec130011ee39c6e5a94f001621457
SebassER158 <escobarreyessebastian@hotmail.com>
2024-11-07 12:41:26 -0600

    cambios sin google services

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+<<<<<<< HEAD
+import file_selector_macos
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+=======
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+>>>>>>> 40a49c2c8d2dacad6791bca80f3c23b81a41b839
+}
---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+<<<<<<< HEAD
+#include <file_selector_windows/file_selector_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+=======
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+>>>>>>> 40a49c2c8d2dacad6791bca80f3c23b81a41b839
+}
